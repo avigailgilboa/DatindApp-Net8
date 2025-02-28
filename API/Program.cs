d11220c716156953afad98042594411e12b3e2c3
avigailgilboa <avigailgilboa967@gmail.com>
2025-02-28 12:57:06 +0200

    End of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -8,16 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationService(builder.Configuration);
 builder.Services.AddIdentityService(builder.Configuration);
 
-
-
-
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
 .WithOrigins("http://localhost:4200","https://localhost:4200"));
 
